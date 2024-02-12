b3991b55ef79c29ff6b606419ca61b4a3c01234e
Stanislav Kostenich <stanislav.kostenich@gmail.com>
2024-02-12 23:51:10 +0200

    Initial commit

---
+++ src/main/java/example/cashcards/CashcardsApplication.java
@@ -0,0 +1,13 @@
+package example.cashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/cashcards/CashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package example.cashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
